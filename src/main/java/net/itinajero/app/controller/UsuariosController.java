9350c3142c4757d3ad6cf04f2ea40164ae3d20c8
polijt223 <pablotulian10@gmail.com>
2019-01-14 08:31:48 -0300

    usando bcrypt
    se realizan modificaciones para usar tablas personalizadas de mysql para guardar las claves encriptadas con bcrypt

--- src/main/java/net/itinajero/app/controller/UsuariosController.java
+++ src/main/java/net/itinajero/app/controller/UsuariosController.java
@@ -1,5 +1,26 @@
 package net.itinajero.app.controller;
 
-public class UsuariosController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/usuarios")
+public class UsuariosController {
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+
+	@GetMapping("/demo-bcrypt")
+	public String pruebaBcrypt() {
+		
+		String password = "mari123";
+		String encriptado = encoder.encode(password);
+		System.out.println("Password encriptado: " + encriptado);
+		return "usuarios/demo";
+		
+	}
 }
+
